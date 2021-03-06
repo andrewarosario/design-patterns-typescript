2a96b7dcaded0583858a6cb18cfe40f0ff0e35e4
Andrew Rosário <andrew.arosario@gmail.com>
2021-03-06 18:33:40 -0300

    remove unused imports

--- src/structural/decorator/main.ts
+++ src/structural/decorator/main.ts
@@ -1,5 +1,4 @@
 import { ProductCustomizationDecorator } from './product/product-customization-decorator';
-import { ProductDecorator } from './product/product-decorator';
 import { ProductStampDecorator } from './product/product-stamp-decorator';
 import { TShirt } from './product/t-shirt';
 
