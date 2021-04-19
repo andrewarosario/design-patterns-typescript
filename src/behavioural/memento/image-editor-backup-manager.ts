2127f947ac66e317c1672e03edfce3dbb47beda4
Andrew Rosário <andrew.arosario@gmail.com>
2021-04-19 11:58:49 -0300

    memento implementation

---
+++ src/behavioural/memento/image-editor-backup-manager.ts
@@ -0,0 +1,30 @@
+import { ImageEditor } from './image-editor';
+import { Memento } from './memento';
+
+export class ImageEditorBackupManager {
+  private mementos: Memento[] = [];
+  constructor(private readonly imageEditor: ImageEditor) {}
+
+  backup(): void {
+    console.log(`Salvando o estado de Image Editor`);
+    this.mementos.push(this.imageEditor.save());
+  }
+
+  undo(): void {
+    const memento = this.mementos.pop();
+
+    if (!memento) {
+      console.log(`Não há backups!`);
+      return;
+    }
+
+    this.imageEditor.restore(memento);
+    console.log(`Backup: ${memento.getName()} foi restaurado com sucesso!`);
+  }
+
+  showMementos(): void {
+    for (const memento of this.mementos) {
+      console.log(memento);
+    }
+  }
+}
